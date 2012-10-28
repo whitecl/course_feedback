4e469e8eb73f498d9cdcef0032ccb43f1a97a62e
Chris White <cwhite@sproutbox.com>
2012-10-28 17:48:42 -0400

    Initial Course Feedback commit from Rails 3.2.8 new generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseFeedback::Application.config.secret_token = 'ecb6f8164fe96364e1bb7550dde8dccb03b966331f4df1e2042982ebdef707bedd22e30fcd9d2e6bef37c49d1e57e4b28b9cc8849f29c9aff7f02640fc1704f2'
