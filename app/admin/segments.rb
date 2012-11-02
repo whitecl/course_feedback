c39a13a00c63efe144a41fa548705cda5649b50d
Chris White <cwhite@sproutbox.com>
2012-11-02 19:58:06 -0400

    Missed Course input on segment admin form

--- app/admin/segments.rb
+++ app/admin/segments.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Segment do
 
     form do |f|
         f.inputs do
+            f.input :course
             f.input :name
             f.input :released
             f.input :attachment, as: :file
